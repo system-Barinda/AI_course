2f67394a6665d3294838f4bbe86fcad408a4c041
system sylvere BARINDA <systembarinda@gmail.com>
2026-03-31 20:02:34 +0200

    done to update all everything

--- panda.config.ts
+++ panda.config.ts
@@ -1,20 +1,18 @@
-import { defineConfig } from "@pandacss/dev";
-
+import { defineConfig } from '@pandacss/dev'
+ 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
-
+ 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
-
+  include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
+ 
   // Files to exclude
   exclude: [],
-
-  // Useful for theme customization
-  theme: {
-    extend: {},
-  },
-
+ 
+  // Generates JSX utilities with options of React, Preact, Qwik, Solid, Vue
+  jsxFramework: 'react',
+ 
   // The output directory for your css system
-  outdir: "styled-system",
-});
+  outdir: 'styled-system'
+})
